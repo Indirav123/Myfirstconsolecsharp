dd43ada2505864d4a6b8f65cfddd172701029386
Indirav123 <Indira.Vundrakonda@ibm.com>
2022-03-01 12:51:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+
+namespace Myfirstconsolecsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to c#");
+            Console.WriteLine("IBM Training");
+            Console.WriteLine("hello");
+            string name="Peter";
+            Console.WriteLine(name);
+            string address = "Hyderabad";
+            //{} string interpolation
+            Console.WriteLine("my name is{0} and I am from {1}", name, address);
+            int val = 9 + 5;
+            Console.WriteLine("val:" + val);
+            double d = 6.187;
+            Char s = 'D';
+            bool t = true;
+            Console.WriteLine("Double:"+d);
+            Console.WriteLine("Char:" + s);
+            Console.WriteLine("Boolean:" + t);
+            //Typecasting 
+            double one = 8.9;
+            int two=(int)one;
+            Console.WriteLine("Explicit typecast:" + one);
+            Console.WriteLine(two);
+            //switch case
+            int colorcode;
+            Console.WriteLine("Enter colorcode");
+            colorcode = int.Parse(Console.ReadLine());
+            switch(colorcode)
+            {
+                case 0: Console.WriteLine("red");
+                    break; 
+                case 1: Console.WriteLine("yellow");
+                    break;
+                default: Console.WriteLine("Invalid");
+                    break;
+            }
+            //task 1:greatest of three numbers
+            int a, b,c;
+            Console.WriteLine("Enter the values of a,b,c");
+            a = int.Parse(Console.ReadLine());
+            b= int.Parse(Console.ReadLine());
+            c= int.Parse(Console.ReadLine());
+            if (a > b&&a>c)
+                Console.WriteLine("a is greater");
+            if (b > a && b > c)
+                Console.WriteLine("b is greater");
+            if(c > a&& c>b)
+                Console.WriteLine("c is greater");
+            //task 2:swaping of 2 numbers withiut third variable
+            int e,f;
+            Console.WriteLine("Enter values");
+            e = int.Parse(Console.ReadLine());
+            f = int.Parse(Console.ReadLine());
+            e = e * f;
+            f = e / f;
+            e = e / f;
+            Console.WriteLine("after swap e="+e+"b="+f);
+            //task 3:employee details
+            string empid,empname;
+            int age, salary;
+            Console.WriteLine("Enter empid,empname,age,salary");
+            empid = Console.ReadLine();
+            empname=Console.ReadLine();
+            age = int.Parse(Console.ReadLine());
+            salary = int.Parse(Console.ReadLine());
+            Console.WriteLine("The details of the employee are empid="+empid+ "empname="+empname+ "age="+age+ "salary="+salary);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ looping.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myfirstconsolecsharp
+{
+    internal class looping
+    {
+        public static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter the number");
+            num=int.Parse(Console.ReadLine());
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int num1;
+            Console.WriteLine("Enter the values of num1");
+            num1 = int.Parse(Console.ReadLine());
+            int j = 0;
+            while (j < num1)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            //do while
+            int num2;
+            Console.WriteLine("Enter the value of num2");
+            num2=int.Parse(Console.ReadLine());
+            int k = 10;
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+            }while(k< num2);            
+            Console.ReadLine();
+        }
+
+    }
+}
